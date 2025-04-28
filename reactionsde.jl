36e0790dc89926912c1c64a85707f83db63cec2c
rafifsr <rafif.ramadhan@rwth-aachen.de>
2025-04-28 12:23:18 +0200

    more noise

--- reactionsde.jl
+++ reactionsde.jl
@@ -19,17 +19,16 @@ end
 function g(du, u, p, t)
     σ1 = p[3]
     σ2 = p[4]
-    σ3 = p[5]
-    # du[1] = σ1*u[1]
-    du[2] = -σ2*u[2] 
+    du[1] = - σ1*u[1]
+    du[2] = σ1*u[1] - σ2*u[2] 
     du[3] = σ2*u[2]
 end
 
 # Initial conditions and parameters
 u0 = [1.0, 0.0, 0.0]  # Initial concentrations of A, B, and C
-T = 60.0             # Final time
+T = 20.0             # Final time
 tspan = (0.0, T)      # Time span
-p = (0.5, 0.3, 0.1, 0.2, 0.1)   # Parameters k1, k2, σ1, σ2
+p = (0.5, 0.5, 0.1, 0.5)   # Parameters k1, k2, σ1, σ2
 
 # Problem definition
 prob = SDEProblem(f, g, u0, tspan, p)
