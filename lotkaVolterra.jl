d3a0e9cc305390c6669bcf109f3835fc98e67dd7
rafifsr <rafif.ramadhan@rwth-aachen.de>
2025-06-07 23:20:11 +0200

    renamed Lotka Volterra

--- lotkaVolterra.jl
+++ lotkaVolterra.jl
@@ -12,13 +12,22 @@ u0 = [1.0; 1.0]
 tspan = (0.0, 10.0)
 p = [1.5]
 prob = ODEProblem(f, u0, tspan, p)
-
 sol = solve(prob, Tsit5())
+
+# Generate synthetic data
 t = collect(range(0, stop = 10, length = 200))
 randomized = VectorOfArray([(sol(t[i]) + 0.01randn(2)) for i in 1:length(t)])
 data = convert(Array, randomized)
 
-newprob = remake(prob, p = [1.42])
+# Define the loss function
+cost_function = build_loss_objective(prob, Tsit5(), L2Loss(t, data),
+                                     Optimization.AutoForwardDiff(),
+                                     maxiters = 10000, verbose = false)
+
+# Define the optimization problem
+optprob = Optimization.OptimizationProblem(cost_function, [1.42])
+optsol = solve(optprob, BFGS())
+newprob = remake(prob, p = optsol.u)
 newsol = solve(newprob, Tsit5())
-plot(sol, label="Original", xlabel="Time", lw=2)
-plot!(newsol, label="New", lw=2, linestyle=:dash)
+plot(sol)
+plot!(newsol, linestyle = :dash, lw=2)
