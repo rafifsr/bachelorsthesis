1d0b47342a35cfa2fa553060f7ce67e65a172e35
rafifsr <rafif.ramadhan@rwth-aachen.de>
2025-06-12 11:44:18 +0200

    working parameter estimation, still not good

--- paramEstimMA.jl
+++ paramEstimMA.jl
@@ -1,9 +1,25 @@
 using Pkg
 Pkg.activate(@__DIR__)
 using DifferentialEquations, RecursiveArrayTools, Plots, DiffEqParamEstim
-using Optimization, ForwardDiff, OptimizationOptimJL, OptimizationBBO
+using Optimization, ForwardDiff, OptimizationOptimJL, OptimizationBBO, OptimizationOptimJL, SciMLSensitivity
+using CSV, DataFrames
 
-function odes!(du, u, p, t)
+# ------------------------------
+# 1. Load Experimental Data
+# ------------------------------
+df = CSV.read("datasetsMA/nitrogenlim.csv", DataFrame)
+df_Xa = df[!, [:time, :Xa]]
+df_Xi = df[!, [:time, :Xi]]
+df_N = df[!, [:time, :N]]
+df_Suc = df[!, [:time, :S]]
+df_FruGlu = df[!, [:time, :FG]]
+df_MA = df[!, [:time, :MA]]
+dfs = [df_Xa, df_Xi, df_N, df_Suc, df_FruGlu, df_MA]
+
+# ------------------------------
+# 2. Define ODE Model
+# ------------------------------
+function f!(du, u, p, t)
     (μmax, KFG, KN, YXa_S, YXi_S, YXa_N, YP_S, ϕ, χacc,
        μ2max, qsplit_max, Ksuc, qpmax, KIP, KIN, KPFG, KFG2) = p
 
@@ -28,26 +44,52 @@ function odes!(du, u, p, t)
     du[6] = qp * Xact
 end
 
-u0 = [1.0; 1.0]
-tspan = (0.0, 10.0)
-p = [1.5]
-prob = ODEProblem(f, u0, tspan, p)
-sol = solve(prob, Tsit5())
+# ------------------------------
+# 3. Prepare Time Grid, Parameters, and Initial Condition
+# ------------------------------
+times = df.time
+t = collect(times)
 
-# Generate synthetic data
-t = collect(range(0, stop = 10, length = 200))
-randomized = VectorOfArray([(sol(t[i]) + 0.01randn(2)) for i in 1:length(t)])
-data = convert(Array, randomized)
+# Assume initial condition from first available values
+u0 = [df_Xa.Xa[1], df_Xi.Xi[1], df_N.N[1], df_Suc.S[1], df_FruGlu.FG[1], df_MA.MA[1]]
 
-# Define the loss function
-cost_function = build_loss_objective(prob, Tsit5(), L2Loss(t, data),
+# Guessed parameters
+p = [
+    0.125,  # μmax
+    0.147,  # KFG
+    0.000038,  # KN
+    0.531,  # YXa_S
+    0.799,  # YXi_S
+    9.428,  # YXa_N
+    0.508,  # YP_S
+    1.56,  # ϕ
+    0.3,  # χacc
+    0.125,  # μ2max
+    1.985,  # qsplit_max
+    0.00321,  # Ksuc
+    0.28188,  # qpmax
+    0.000147,  # KIP
+    0.000147,  # KIN
+    0.0175,  # KPFG
+    3.277  # KFG2
+]
+# ------------------------------
+# 4. Define Loss Function and Optimization Problem
+# ------------------------------
+prob = ODEProblem(f!, u0, (0.0, maximum(t)), p)
+
+cost_function = build_loss_objective(prob, Tsit5(), L2Loss(t, df),
                                      Optimization.AutoForwardDiff(),
                                      maxiters = 10000, verbose = false)
 
-# Define the optimization problem
-optprob = Optimization.OptimizationProblem(cost_function, [1.42])
+optprob = Optimization.OptimizationProblem(cost_function, p)
 optsol = solve(optprob, BFGS())
 newprob = remake(prob, p = optsol.u)
 newsol = solve(newprob, Tsit5())
-plot(sol)
-plot!(newsol, linestyle = :dash, lw=2)
+println("Optimized parameters: ", optsol.u)
+
+# ------------------------------
+# 5. Plot FruGlu Results (experimental data vs guessed parameters vs optimized)
+# ------------------------------
+plot(df.time, df.FG, label = "Experimental FruGlu", xlabel = "Time", ylabel = "FruGlu", title = "FruGlu Concentration Over Time")
+plot!(newsol.t, newsol[5, :], label = "Optimized FruGlu", linestyle = :dash)
